ee00bfb0adabbe8e6aa6419aceb3129d7956c020
engelisart <144841027+engelisart@users.noreply.github.com>
2023-12-10 19:48:39 +0100

    fixed api to componentDidUpdate & esc keydown

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { SearchBar } from '../components/Searchbar/Searchbar';
 import ButtonLoadMore from '../components/Button/Button';
 import { Loader } from './Loader/Loader';
-import { fetchImg } from './api';
+import { fetchImg } from 'api';
 import ImageGallery from './ImageGallery/ImageGallery';
 import { Modal } from './Modal/Modal';
 
 export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
   state = {
     hits: [],
     total: 0,
@@ -19,12 +24,56 @@ export class App extends React.Component {
   };
 
   async componentDidMount() {
+    window.addEventListener('keydown', this.closeModal);
     const data = await fetchImg();
     this.setState(prevState => ({
       hits: data.hits,
       total: data.total,
       totalHits: data.totalHits,
     }));
+  }
+
+  async componentDidUpdate(_, prevState) {
+    if (this.state.searchQuery !== prevState.searchQuery) {
+      try {
+        this.setState({ loading: true });
+        const data = await fetchImg({ q: this.state.searchQuery });
+        this.setState({
+          hits: data.hits,
+          total: data.total,
+          totalHits: data.totalHits,
+        });
+        this.setState({ loading: false });
+      } catch (error) {
+        console.log('Error fetching data', error);
+      }
+    } else if (this.state.page !== prevState.page) {
+      try {
+        this.setState({ loading: true });
+        const data = await fetchImg({
+          q: this.state.searchQuery,
+          page: this.state.page,
+        });
+
+        this.setState(prevState => ({
+          hits: [...prevState.hits, ...data.hits],
+          total: data.total,
+          totalHits: data.totalHits,
+        }));
+        this.setState({ loading: false });
+      } catch (error) {
+        console.log('Error fetching data', error);
+      }
+    }
+  }
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.closeModal);
+  }
+
+  closeModal(event) {
+    if (event.key === 'Escape') {
+      this.setState({ activeImage: null });
+    }
   }
 
   showLoadMore = () => {
@@ -46,42 +95,11 @@ export class App extends React.Component {
   handleLoadMore = async () => {
     await this.setState(prevState => ({
       page: prevState.page + 1,
-      loading: true,
     }));
-
-    try {
-      const data = await fetchImg({
-        q: this.state.searchQuery,
-        page: this.state.page,
-      });
-
-      this.setState(prevState => ({
-        hits: [...prevState.hits, ...data.hits],
-        total: data.total,
-        totalHits: data.totalHits,
-      }));
-      this.setState({ loading: false });
-    } catch (error) {
-      console.log('Error fetching data', error);
-    }
-
-    console.log(this.showLoadMore());
   };
 
   handleSearchSubmit = async query => {
     this.setState({ searchQuery: query, page: 1, hits: [], loading: true });
-
-    try {
-      const data = await fetchImg({ q: query });
-      this.setState({
-        hits: data.hits,
-        total: data.total,
-        totalHits: data.totalHits,
-      });
-      this.setState({ loading: false });
-    } catch (error) {
-      console.log('Error fetching data', error);
-    }
   };
 
   render() {
